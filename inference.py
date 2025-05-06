883d76533b7b9caeb327d74f5a03f96f9abb954d
Erdem Sengün <erdem.senguen@th-owl.de>
2025-05-06 12:53:32 +0200

    new try

--- inference.py
+++ inference.py
@@ -42,6 +42,7 @@ class Inference:
         self.log_counter_face_success=0
         self.log_counter_cam_dupe=0
         self.log_counter_cam_dupe_success=0
+        self.black_image = np.zeros((1080, 1920, 3), dtype=np.uint8)
         self.conf_virt_live_webcam()
     def partial_fields(self,target_class, kwargs):
         return target_class(**{k: v for k, v in kwargs.items() if hasattr(target_class, k)})
@@ -49,11 +50,10 @@ class Inference:
     def main(self):
         with pyvirtualcam.Camera(width=1920, height=1080, fps=30, backend='v4l2loopback', device='/dev/video10') as cam, \
              pyvirtualcam.Camera(width=1920, height=1080, fps=30, backend='v4l2loopback', device='/dev/video11') as cam2:
-            black_image = np.zeros((1080, 1920, 3), dtype=np.uint8)
             while True:
                 if not self.running:
-                    cam.send(black_image)
-                    cam2.send(black_image)
+                    cam.send(self.black_image)
+                    cam2.send(self.black_image)
                     continue
                 else:
                     break
@@ -74,66 +74,60 @@ class Inference:
                 cam2.send(frame_fhd)
                 frame_small= cv2.resize(frame,(640,640))
                 is_face = face_detector(frame_small)
-                if self.first_iter == True and self.source_image_path!=None:
+                if self.first_iter and self.source_image_path:
                     self.logger.debug("DeepFake source image is set!")
-                    x_s, f_s, R_s, x_s_info, lip_delta_before_animation, crop_info, img_rgb = self.live_portrait_pipeline.execute_frame(frame, self.source_image_path)
+                    self.x_s, self.f_s, self.R_s, self.x_s_info, self.lip_delta_before_animation, self.crop_info, self.img_rgb = self.live_portrait_pipeline.execute_frame(frame, self.source_image_path)
                     self.first_iter=False
                 # Process the frame
-                if is_face and self.source_image_path:
-                    self.log_counter_cam_dupe=0
-                    self.log_counter_cam_dupe_success=0
+                if is_face: 
                     if self.log_counter_face_start==0:
-                        self.logger.debug("Face recognized, running the face manipulation.")
-                        self.log_counter_face_start+=1
-                    self.active=True
-                    pad=black_image
-                    result = self.live_portrait_pipeline.generate_frame(x_s, f_s, R_s, x_s_info, lip_delta_before_animation, crop_info, img_rgb, frame)
-                    result_height,result_width=result.shape[:2]
-                    if result_height>1080:
-                        result=cv2.resize(result,(int(result_width*1080/result_height),1080))
-                    x_offset=(1920-result_width)//2
-                    y_offset=(1080-result_height)//2
-                    pad[y_offset:y_offset+result_height,x_offset+result_width]=result
-                    if self.log_counter_face_success==0:
-                        self.logger.debug("Face control established.")
-                        self.log_counter_face_success+=1
-                    cam.send(pad)
+                            self.logger.debug("Face found.")
+                            self.log_counter_face_start+=1
+                    if self.source_image_path:
+                        self.manipulation(cam=cam,frame=frame_fhd)
                 else:
-                    self.log_counter_face_success=0
-                    self.log_counter_face_start=0
-                    if self.log_counter_cam_dupe==0:
-                        self.logger.debug("Face not found, duplicating the cam input.")
-                        self.log_counter_cam_dupe+=1
-                    self.active=False
-                    self.source_image_path=None
-                    self.first_iter=True
-                    overlay_resized = cv2.resize(self.overlay, (frame_fhd.shape[1], frame_fhd.shape[0]))
-                    overlay_rgb = overlay_resized[..., :3]
-                    try:
-                        alpha_mask = overlay_resized[..., 3:]/255
-                    except Exception as e:
-                        alpha_mask= np.full((1080,1920),0.4)
-                        self.logger.error(e) 
-                    blended = (1.0 - alpha_mask) * frame_fhd + alpha_mask * overlay_rgb
-                    blended = blended.astype(np.uint8)
-                    blended = cv2.resize(blended,(1920,1080))
-                    if self.log_counter_cam_dupe_success==0:
-                        self.logger.debug("Duplicated camera feed is succesful.")
-                        self.log_counter_cam_dupe_success+=1
-                    cam.send(blended)
-                #cv2.imshow('img_rgb Image', img_rgb)
-                #cv2.imshow('Source Frame', frame)
-                
-
-                # [Key Change] Convert the result from RGB to BGR before displaying
-
-                # Display the resulting frame
-                #cv2.imshow('Live Portrait', result_bgr)
-
-                # Press 'q' to exit the loop
-            # When everything is done, release the capture
+                    self.no_manipulation(cam=cam,frame=frame_fhd)
             cap.release()        
         # live_portrait_pipeline.execute_frame(result_bgr)
+    def manipulation(self,cam,frame):
+        self.log_counter_cam_dupe=0
+        self.log_counter_cam_dupe_success=0
+        self.active=True
+        pad=self.black_image
+        result = self.live_portrait_pipeline.generate_frame(self.x_s, self.f_s, self.R_s, self.x_s_info, self.lip_delta_before_animation, self.crop_info, self.img_rgb, frame)
+        result_height,result_width=result.shape[:2]
+        if result_height>1080:
+            result=cv2.resize(result,(int(result_width*1080/result_height),1080))
+        x_offset=(1920-result_width)//2
+        y_offset=(1080-result_height)//2
+        pad[y_offset:y_offset+result_height,x_offset+result_width]=result
+        if self.log_counter_face_success==0:
+            self.logger.debug("Face control established.")
+            self.log_counter_face_success+=1
+        cam.send(pad)
+    def no_manipulation(self,cam,frame):
+        self.log_counter_face_success=0
+        self.log_counter_face_start=0
+        if self.log_counter_cam_dupe==0:
+            self.logger.debug("Face not found, duplicating the cam input.")
+            self.log_counter_cam_dupe+=1
+        self.active=False
+        self.source_image_path=None
+        self.first_iter=True
+        overlay_resized = cv2.resize(self.overlay, (frame.shape[1], frame.shape[0]))
+        overlay_rgb = overlay_resized[..., :3]
+        try:
+            alpha_mask = overlay_resized[..., 3:]/255
+        except Exception as e:
+            alpha_mask= np.full((1080,1920),0.4)
+            self.logger.error(e) 
+        blended = (1.0 - alpha_mask) * frame + alpha_mask * overlay_rgb
+        blended = blended.astype(np.uint8)
+        blended = cv2.resize(blended,(1920,1080))
+        if self.log_counter_cam_dupe_success==0:
+            self.logger.debug("Duplicated camera feed is succesful.")
+            self.log_counter_cam_dupe_success+=1
+        cam.send(blended)
     def conf_virt_live_webcam(self):
         if platform.system() == "Windows":
             self.backend = "obs"
@@ -145,7 +139,8 @@ class Inference:
         self.first_iter=True
         try:
             load_image_rgb(source_img_path)
-            self.source_img_path=source_img_path
+            self.source_image_path=source_img_path
+            self.logger.debug("Image set successfully!")
             return "Image set successfully."
         except Exception as e:
             self.source_image_path=None
