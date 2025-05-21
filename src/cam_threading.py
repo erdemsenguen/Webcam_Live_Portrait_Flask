72458da6b9adf3e351ca4a6e6e0ab9493622caba
Erdem Sengün <erdem.senguen@th-owl.de>
2025-05-21 15:33:41 +0200

    cam fix

--- src/cam_threading.py
+++ src/cam_threading.py
@@ -3,14 +3,20 @@ import threading
 class WebcamStream:
     def __init__(self, src=0):
         self.cap = cv2.VideoCapture(src)
+        if not self.cap.isOpened():
+            raise RuntimeError("Error: Unable to open video source", src)
         self.ret, self.frame = self.cap.read()
+        if not self.ret or self.frame is None:
+            raise RuntimeError("Error: Unable to read from the camera.")
+        else:
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH,800)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT,450)
         self.running = True
         self.thread=threading.Thread(target=self.update)
         self.thread.start()
     def update(self):
         while self.running:
             self.ret, self.frame = self.cap.read()
-
     def read(self):
         return self.ret, self.frame
     def set(self,prop_id,value):
