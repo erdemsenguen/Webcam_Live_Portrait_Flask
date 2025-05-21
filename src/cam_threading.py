d6059c73348ba41ae0061c8c3c094cd83657798e
Erdem Sengün <erdem.senguen@th-owl.de>
2025-05-21 15:38:04 +0200

    error chasing

--- src/cam_threading.py
+++ src/cam_threading.py
@@ -1,13 +1,15 @@
 import cv2
 import threading
+import logging
 class WebcamStream:
     def __init__(self, src=0):
+        self.logger=logging.getLogger(__name__)
         self.cap = cv2.VideoCapture(src)
         if not self.cap.isOpened():
-            raise RuntimeError("Error: Unable to open video source", src)
+            self.logger.error("Error: Unable to open video source", src)
         self.ret, self.frame = self.cap.read()
         if not self.ret or self.frame is None:
-            raise RuntimeError("Error: Unable to read from the camera.")
+            self.logger.error("Error: Unable to read from the camera.")
         else:
             self.cap.set(cv2.CAP_PROP_FRAME_WIDTH,800)
             self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT,450)
